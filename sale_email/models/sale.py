89cc70f099d317bcb27d2013c2bc9f1243b98a5c
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2016-09-01 17:20:58 +0200

    [MIG] migrate code from 7.0

---
+++ sale_email/models/sale.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# © 2013-TODAY Akretion (http://www.akretion.com)
+#   @author Florian DA COSTA <florian.dacosta@akretion.com>
+#   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import time
+
+from openerp import api, fields, models
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+      
+
+    @api.multi
+    def action_quotation_send(self):
+        '''
+        This function opens a window to compose an email, with the edi sale template message loaded by default
+        '''
+        self.ensure_one()
+        action_dict = super(ActionsServer, self).action_quotation_send()
+        if self.env.context.get('send_email'):
+            ctx = action_dict['context']
+            template_id = False
+            ir_model_data = self.env['ir.model.data']
+            try:
+                template_id = self.ref(
+                    'sale_email.email_template_sale_confirmation')
+            except ValueError:
+                template_id = False
+            if template_id:
+               ctx.update({
+                'default_use_template': bool(template_id),
+                'default_template_id': template_id,
+                })
+                action_dict.update({'context': ctx})
+        return action_dict
+
+
+
+
