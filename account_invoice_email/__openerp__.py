c548e3aebd562db9d284d58180f58a68756e99fb
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2016-09-02 16:44:47 +0200

    [WIP] add force mail send method change manifest

--- account_invoice_email/__openerp__.py
+++ account_invoice_email/__openerp__.py
@@ -1,45 +1,26 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-#   action_server_email for OpenERP
-#   Copyright (C) 2013-TODAY Akretion <http://www.akretion.com>.
+# © 2013-TODAY Akretion (http://www.akretion.com)
 #   @author Florian DA COSTA <florian.dacosta@akretion.com>
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as
-#   published by the Free Software Foundation, either version 3 of the
-#   License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
-
+#   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Stock Invoice Email',
-    'version': '0.1',
+    'name': 'Account Invoice Email',
+    'version': '9.0.1.0.0',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
     'description': 
     """
-        Send an automatic email when an invoice is created in order to confirm an expedition.
+        Send an automatic email when an invoice is validated.
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com/',
     'depends': [
         "action_server_email",
-        "stock"
+        "account"
     ], 
     'data': [ 
-        'invoice_data.xml',
-        'invoice_workflow.xml',
+        'data/invoice_data.xml',
     ],
-    'demo': [],
     'installable': True,
 }
