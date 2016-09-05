81ef74d16a1624cd1fd450fb61039cf7c6cf7718
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2016-09-05 17:14:41 +0200

    [ADD] add sale_email module

---
+++ sale_email/models/sale.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2013-TODAY Akretion (http://www.akretion.com)
+#   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = ['sale.order', 'automatic.mail.mixin']
+    _name = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        self.force_confirm_mail_send()
+        return res
