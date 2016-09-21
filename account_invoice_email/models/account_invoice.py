47a5a20aea7d9b98556156f7b4640e8796765256
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2016-09-21 10:14:10 +0200

    [FIX] add test should_send_mail to invoice

--- account_invoice_email/models/account_invoice.py
+++ account_invoice_email/models/account_invoice.py
@@ -10,11 +10,20 @@ class AccountInvoice(models.Model):
     _inherit = ['account.invoice', 'automatic.mail.mixin']
     _name = 'account.invoice'
 
+    def should_send_mail(self):
+        res = super(AccountInvoice, self).should_send_mail()
+        # send confirmation mail only for Customer invoices
+        selected_inv = res.filtered(
+            lambda r: r.type == 'out_invoice')
+        if selected_inv:
+            return selected_inv
+        else:
+            return False
+
     @api.multi
     def invoice_validate(self):
         res = super(AccountInvoice, self).invoice_validate()
-        # send confirmation mail only for Customer invoices
-        out_invoice = self.filtered(
-                lambda r: r.type == 'out_invoice')
-        out_invoice.force_confirm_mail_send()
+        selected_inv = self.should_send_mail()
+        if selected_inv:
+            selected_inv.force_confirm_mail_send()
         return res
